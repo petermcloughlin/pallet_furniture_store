8c03087bf9185121b540f177d2b9db64e5480271
Peter Mcloughlin <mcloughlin78@gmail.com>
2025-01-26 17:12:47 +0000

    Add filtering by category and product type for nav dropdowns

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import StoreProduct
+from .models import StoreProduct, ProductCategory
 
 # Create your views here.
 
@@ -9,9 +9,15 @@ def all_products(request):
     ''' A view to display all products '''
 
     products = StoreProduct.objects.all()
+    categories = None
     query = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = ProductCategory.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +30,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
