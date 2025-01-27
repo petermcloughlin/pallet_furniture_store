683ad4011291457d1826ba331537068e800f41c4
Peter Mcloughlin <mcloughlin78@gmail.com>
2025-01-27 10:59:44 +0000

    Add sorting by price ascending and descending for navbar

--- products/views.py
+++ products/views.py
@@ -11,6 +11,8 @@ def all_products(request):
     products = StoreProduct.objects.all()
     categories = None
     query = None
+    sort = None
+    direction = None
 
     if request.GET:
         if 'category' in request.GET:
@@ -27,10 +29,26 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
