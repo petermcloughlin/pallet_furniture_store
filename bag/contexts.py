758c97bdff52ebbb3b6c06f77bbf34ad894a9c46
petermcloughlin <mcloughlin78@gmail.com>
2025-01-31 23:26:54 +0000

    Added shopping bag html template content

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import StoreProduct
 
 def bag_contents(request):
 
@@ -5,12 +8,26 @@ def bag_contents(request):
     total = 0
     product_count = 0 
     delivery = 10
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(StoreProduct, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    grand_total = total
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
-        'delivery': delivery
+        'delivery': delivery,
+        'grand_total': grand_total,
     }
 
     return context
