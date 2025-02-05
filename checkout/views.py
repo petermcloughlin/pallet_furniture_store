4f0c72af6ce68936cb1c8c0050be91fcc87959ca
petermcloughlin <mcloughlin78@gmail.com>
2025-02-05 16:42:27 +0000

    Added some basic card payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import CustomerOrderForm
+from bag.contexts import bag_contents
 
+import stripe
 # Create your views here.
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'There is nothing in your shopping bag.')
         return redirect(reverse('products'))
     
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    
     order_form = CustomerOrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+                         Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Qp6L7R0KkM8XZH6VLWDyO41n3tyel7XdyqigatIqFl67Q5BVdIXTYqENXDjkpq84eXlORDRsOTXqfWEPP2zVPTi006yWdKGgJ',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
