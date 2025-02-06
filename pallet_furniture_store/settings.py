2bc98c96476c497d2569cb0a71889067ea927ef1
petermcloughlin <mcloughlin78@gmail.com>
2025-02-06 19:00:38 +0000

    Added payment data caching to the webhooks

--- pallet_furniture_store/settings.py
+++ pallet_furniture_store/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)k2)kzf^r1qkl*z)u-(un&9()sbe5l6pqrd4g^759ox^vg^c^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com', 'localhost',]
 
