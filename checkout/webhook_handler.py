460c7a324aac1debd015255653bcf99ecef5c43c
petermcloughlin <mcloughlin78@gmail.com>
2025-02-07 10:59:23 +0000

    Completed webhook functionality, fixed some issues surrounding 401 and 500 errors, updated wh handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from .models import CustomerOrder, CustomerOrderLineItem
 from products.models import StoreProduct
 
+import stripe
 import json
 import time
 
@@ -28,9 +29,14 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        billing_details = intent.charges.data[0].billing_details
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
+        billing_details = stripe_charge.billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2)
 
         for field, value in shipping_details.address.items():
             if value == "":
