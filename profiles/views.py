09778774294bf945dbc4f8b33f8a86920e34732c
petermcloughlin <mcloughlin78@gmail.com>
2025-02-10 23:45:45 +0000

    Added customer order history and checkout view with user profile info

--- profiles/views.py
+++ profiles/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import CustomerOrder
 
 
 # Create your views here.
 def profile(request):
-    ''' Dispplay the user profile '''
+    ''' Display the user profile '''
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
@@ -25,4 +26,25 @@ def profile(request):
         'orders': orders,     
         'on_profile_page': True,
     }
+    
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    ''' display order history of the user '''
+
+    order = get_object_or_404(CustomerOrder, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}.'
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
     return render(request, template, context)
