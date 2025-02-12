2961ef17c156d60ae2a54997865e0cba7ec4d33f
petermcloughlin <mcloughlin78@gmail.com>
2025-02-12 14:39:31 +0000

    Add more functionality to product management page to allow admin user add a product to store

--- products/views.py
+++ products/views.py
@@ -72,8 +72,18 @@ def product_detail(request, storeproduct_id):
 
 
 def add_product(request):
-    """ Add a product to the store - for admin user"""
-    form = ProductForm()
+    """ Add a product to the store - for admin user""" 
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+            
     template = 'products/add_product.html'
     context = {
         'form': form,
