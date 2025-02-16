718fb49c9d44f2c1d8d1cb8410a9af94315bf092
petermcloughlin <mcloughlin78@gmail.com>
2025-02-16 16:18:32 +0000

    Added missing context processor to settings.py for media files

--- pallet_furniture_store/settings.py
+++ pallet_furniture_store/settings.py
@@ -80,9 +80,10 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', 
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents'
             ],
             'builtins': [
